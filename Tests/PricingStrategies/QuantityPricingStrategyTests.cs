2c4988d0d829c8bda6223f1218ea6c8a767eec16
georgi.orozov <georgi.orozov@gmail.com>
2023-04-03 22:42:38 +0100

    added unit tests for the quantity pricing strategy

--- Tests/PricingStrategies/QuantityPricingStrategyTests.cs
+++ Tests/PricingStrategies/QuantityPricingStrategyTests.cs
@@ -1,6 +1,42 @@
+using System;
+using checkout_kata.Exceptions;
+using checkout_kata.PricingStrategies;
+using Moq;
+using Xunit;
+
 namespace Tests.PricingStrategies;
 
 public class QuantityPricingStrategyTests
 {
-    
+    [Theory]
+    [InlineData(50, 3, 130, 2, 100)]
+    [InlineData(50, 3, 130, 3, 130)]
+    [InlineData(50, 3, 130, 4, 180)]
+    [InlineData(30, 2, 45, 1, 30)]
+    [InlineData(30, 2, 45, 2, 45)]
+    [InlineData(30, 2, 45, 3, 75)]
+    public void Should_CalculatePrice_ReturnCorrectPrice(int singlePrice, int promoQuantity, int promoPrice, int actualQuantity, int expectedPrice)
+    {
+        // Arrange
+        var quantityStrategy = new QuantityPricingStrategy(singlePrice, promoQuantity, promoPrice);
+
+        // Act
+        
+        // Assert
+        Assert.Equal(expectedPrice, quantityStrategy.CalculatePrice(actualQuantity));
+    }
+
+    [Fact]
+    public void Should_CalculatePrice_ThrowExceptionWhenSinglePriceIsZero()
+    {
+        // Arrange
+        var quantityStrategy = new QuantityPricingStrategy(0, It.IsAny<int>(), It.IsAny<int>());
+        
+        // Act
+        Action act = () => quantityStrategy.CalculatePrice(It.IsAny<int>());
+        
+        // Assert
+        var exception = Assert.Throws<CustomException>(act);
+        Assert.Equal("Price cannot be 0", exception.Message);
+    }
 }
--- checkout-kata/PricingStrategies/QuantityPricingStrategy.cs
+++ checkout-kata/PricingStrategies/QuantityPricingStrategy.cs
@@ -4,6 +4,17 @@ namespace checkout_kata.PricingStrategies;
 
 public class QuantityPricingStrategy : IPricingStrategy
 {
+    private int _singlePrice;
+    private int _promoQuantity;
+    private int _promoPrice;
+
+    public QuantityPricingStrategy(int singlePrice, int promoQuantity, int promoPrice)
+    {
+        _singlePrice = singlePrice;
+        _promoQuantity = promoQuantity;
+        _promoPrice = promoPrice;
+    }
+
     public int CalculatePrice(int itemQuantity)
     {
         throw new NotImplementedException();
